8b6492edb7d881affa5a3f9fa10e295164084f25
daniel kafer <daniel.j.kafer@gmail.com>
2017-02-20 14:19:22 -0600

    added recipe details endpoint

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const fetch = require('isomorphic-fetch');
 const app = express();
 const cors = require('cors')
+const key = process.env.MASHAPE_KEY
 
 app.use(cors());
 
 
 // Random Recipe
 app.get('/random', (req, res) => {
-	let key = process.env.MASHAPE_KEY
 	const request = new Request('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random', {
 		'X-Mashape-Key': key,
 		'Content-Type': 'Application/json',
@@ -22,7 +22,26 @@ app.get('/random', (req, res) => {
 		return res.json(data);
 	})
 	.catch( (err) =>{
-		return err;
+		return res.json(err);
+	});
+})
+
+// Recipe Details
+app.get('/recipe/:id', (req, res) => {
+	let id = req.params.id;
+	const request = new Request('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/' + id + '/information', {
+		'X-Mashape-Key': key,
+		'Accept': 'Application/json'
+	});
+	fetch(request)
+	.then( (response) => {
+		return response.json();
+	})
+	.then( (data) => {
+		return res.json(data);
+	})
+	.catch( (err) => {
+		return res.json(err);
 	});
 })
 
