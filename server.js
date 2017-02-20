5ed1d9f8a4d90b252be43701e17fe3852febcfa2
daniel kafer <daniel.j.kafer@gmail.com>
2017-02-20 14:00:52 -0600

    added basic npm packages and random recipe endpoint

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fetch = require('fetch');
+const app = express();
+const cors = require('cors')
+
+app.use(cors());
+
+
+// Random Recipe
+app.get('/random', (req, res) => {
+	const request = new Request('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random', {
+		'X-Mashape-Key': process.env.MASHAPE_KEY,
+		'Content-Type': 'Application/json',
+		'Accept': 'Application/json'
+	});
+	fetch(request)
+	.then( (response) => {
+		return response.json();
+	})
+	.then( (data) => {
+		return res.json(data);
+	})
+	.catch( (err) =>{
+		return err;
+	});
+})
